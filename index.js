9b8e1cbd49c494b2ab328d0ec1b829fa84be0680
azka5123 <115395179+azka5123@users.noreply.github.com>
2024-06-29 18:33:59 +0700

    docs

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParse = require('body-parser');
 const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./docs/swagger_output.json');
 
 dotenv.config();
 
@@ -13,11 +15,13 @@ app.use(cors());
 app.use(bodyParse.json());
 app.use(express.urlencoded({extended: true}));
 
+app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use('/',routes);
 
-// app.get('/',(req,res)=>{
-//     res.status(201).json({msg:'api untuk laundry'});
-// })
+app.get('/',(req,res)=>{
+    res.status(201).json({msg:'api untuk laundry'});
+})
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
