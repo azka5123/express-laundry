28dda5876c7ddf40b901c0b78432e30b22ed6fe0
azka5123 <115395179+azka5123@users.noreply.github.com>
2024-06-29 18:13:32 +0700

    docs api

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const checkAuth = require('../middleware/checkAuth')
 const authRoutes = require('./auth');
 const userRoutes = require('./user');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('../docs/swagger_output.json');
 
 const router = express.Router();
+
+router.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 router.use('/auth',authRoutes);
 
 router.use('/user',checkAuth,userRoutes);
-
-router.get('/test',checkAuth,(req,res)=>{
-    res.json({msg:'berhasil menggunakan token'});
-});
 
 module.exports = router;
