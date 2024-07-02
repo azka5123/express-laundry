c5f8f09335ddc5db2b7eac977e18722d97f8e8b4
azka5123 <115395179+azka5123@users.noreply.github.com>
2024-07-02 19:27:10 +0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -3,28 +3,38 @@ const config = require('../config/config.js');
 
 let sequelize;
 
-if (process.env.NODE_ENV === 'development') {
-  sequelize = new Sequelize(
-    config.development.database,
-    config.development.username,
-    config.development.password,
-    {
-      host: config.development.host,
-      dialect: config.development.dialect,
-    }
-  );
-
-} else if (process.env.NODE_ENV === 'production') {
-  sequelize = new Sequelize(config.production.uri, {
-    dialect: config.production.dialect,
-    dialectModule: require('pg'),
-    dialectOptions: {
-      ssl: {
-        require: true,
-      },
+sequelize = new Sequelize("postgres://default:zhEwJ7Na0jfA@ep-odd-flower-a10yvus9-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require", {
+  dialect: 'postgres',
+  dialectModule: require('pg'),
+  dialectOptions: {
+    ssl: {
+      require: true,
     },
-  });
-}
+  },
+});
+
+// if (process.env.NODE_ENV === 'development') {
+//   sequelize = new Sequelize(
+//     config.development.database,
+//     config.development.username,
+//     config.development.password,
+//     {
+//       host: config.development.host,
+//       dialect: config.development.dialect,
+//     }
+//   );
+
+// } else if (process.env.NODE_ENV === 'production') {
+//   sequelize = new Sequelize(config.production.uri, {
+//     dialect: config.production.dialect,
+//     dialectModule: require('pg'),
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//       },
+//     },
+//   });
+// }
 
 const db = {};
 
