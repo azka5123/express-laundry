ba416a92adb4837ecc523e2527759241178c4a95
azka5123 <115395179+azka5123@users.noreply.github.com>
2024-09-28 20:14:30 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ const router = express.Router();
 // sdas
 // asagfag
 // asfasf
+// daw
 router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
     customCss:
         '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
