b3d3d39654eefa5bdd3eff8eaa2dc982ed5d24a3
azka5123 <115395179+azka5123@users.noreply.github.com>
2024-09-28 20:11:25 +0700

    Reapply "Update index.js"
    This reverts commit 3b09096d3454ba4eb6107ff3eadbf05d6b1608ad.
    
    Update index.js
    
    Revert "Update index.js"
    
    This reverts commit 95cc70956906e9571083c534ab073a4f91e0871d.

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const swaggerSpec = require('../docs/swagger_output.json');
 const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 const router = express.Router();
-
+// sdas
 router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
     customCss:
         '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
